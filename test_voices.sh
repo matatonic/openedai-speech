90e69b074a3031ba5496ce30f3f7e7c8d7339a34
matatonic <matatonic-git@zhero.org>
2023-11-29 03:30:59 -0500

    don't hardcode the url

--- test_voices.sh
+++ test_voices.sh
@@ -1,17 +1,19 @@
-#!/bin/sh
+#!/bin/bash
 
-curl -s http://localhost:8000/v1/audio/speech  -H "Content-Type: application/json"   -d "{
+URL=${1:-http://localhost:8000/v1/audio/speech}
+
+curl -s $URL -H "Content-Type: application/json" -d "{
     \"model\": \"tts-1\",
     \"input\": \"I'm going to play you the original voice, followed by the piper voice and finally the X T T S version 2 voice\",
     \"voice\": \"echo\",
     \"speed\": 1.0
   }" | mpv --really-quiet -
 
-for voice in  alloy echo fable onyx nova shimmer ; do
+for voice in alloy echo fable onyx nova shimmer ; do
 
 echo $voice
 
-curl -s http://localhost:8000/v1/audio/speech  -H "Content-Type: application/json"   -d "{
+curl -s $URL -H "Content-Type: application/json" -d "{
     \"model\": \"tts-1\",
     \"input\": \"original\",
     \"voice\": \"echo\",
@@ -20,14 +22,14 @@ curl -s http://localhost:8000/v1/audio/speech  -H "Content-Type: application/jso
 
 curl -s https://cdn.openai.com/API/docs/audio/$voice.wav | mpv --really-quiet -
 
-curl -s http://localhost:8000/v1/audio/speech  -H "Content-Type: application/json"   -d "{
+curl -s $URL -H "Content-Type: application/json" -d "{
     \"model\": \"tts-1\",
     \"input\": \"The quick brown fox jumped over the lazy dog. This voice is called $voice, how do you like this voice?\",
     \"voice\": \"$voice\",
     \"speed\": 1.0
   }" | mpv --really-quiet -
 
-curl -s http://localhost:8000/v1/audio/speech  -H "Content-Type: application/json"   -d "{
+curl -s $URL -H "Content-Type: application/json" -d "{
     \"model\": \"tts-1-hd\",
     \"input\": \"The quick brown fox jumped over the lazy dog. This HD voice is called $voice, how do you like this voice?\",
     \"voice\": \"$voice\",
@@ -36,28 +38,28 @@ curl -s http://localhost:8000/v1/audio/speech  -H "Content-Type: application/jso
 
 done
 
-curl -s http://localhost:8000/v1/audio/speech  -H "Content-Type: application/json"   -d "{
+curl -s $URL -H "Content-Type: application/json" -d "{
     \"model\": \"tts-1\",
     \"input\": \"the slowest voice\",
     \"voice\": \"onyx\",
     \"speed\": 0.25
   }" | mpv --really-quiet -
 
-curl -s http://localhost:8000/v1/audio/speech  -H "Content-Type: application/json"   -d "{
+curl -s $URL -H "Content-Type: application/json" -d "{
     \"model\": \"tts-1-hd\",
     \"input\": \"the slowest HD voice\",
     \"voice\": \"onyx\",
     \"speed\": 0.25
   }" | mpv --really-quiet -
 
-curl -s http://localhost:8000/v1/audio/speech  -H "Content-Type: application/json"   -d "{
+curl -s $URL -H "Content-Type: application/json" -d "{
     \"model\": \"tts-1\",
     \"input\": \"And this is how fast it can go, the fastest voice\",
     \"voice\": \"nova\",
     \"speed\": 4.0
   }" | mpv --really-quiet -
 
-curl -s http://localhost:8000/v1/audio/speech  -H "Content-Type: application/json"   -d "{
+curl -s $URL -H "Content-Type: application/json" -d "{
     \"model\": \"tts-1-hd\",
     \"input\": \"And this is how fast it can go, the fastest HD voice\",
     \"voice\": \"nova\",
